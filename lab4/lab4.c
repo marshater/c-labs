924befa426abeb508bc33cd836acdd1d4c0929e7
Evgeniy Belyaev <44948688+marshater@users.noreply.github.com>
2019-09-11 01:48:19 +0700

    ошметки 4 лабы

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	FILE *stream;
+	fp = fopen(argv[1], "a");
+	stream = fopen("doub", "w");
+char ch;
+while((ch=fgetc(fp)) != EOF){
+	if (ch == '\n'){
+		fputc(ch, stream);
+		}
+	}
+fclose(fp);
+fclose(stream);
+	return 0;
+}
 
