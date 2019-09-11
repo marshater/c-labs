18e1b6910aa3a0bb2a25df3a5a3124e361946d8c
Evgeniy Belyaev <44948688+marshater@users.noreply.github.com>
2019-09-12 02:59:47 +0700

    Update lab4.c
    WIP

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -6,15 +6,14 @@ int main(int argc, char *argv[])
 	FILE *stream;
 	printf(argv[1],"first\n");
 	printf(argv[2],"second\n");
-	printf(argv[3],"third\n");
 	fp = fopen(argv[1], "r+");
 	stream = fopen("doub", "w");
-char *ch;
-while ((*ch=fgetc(fp)) != EOF){
-	if (*ch == *argv[2]){
+	char ch;
+while ((ch=fgetc(fp)) != EOF){
+	if (ch == *argv[2]){
 		do{
-		fputc(*ch++, stream);
-		}while (*ch == '\n');
+		fputc(ch, stream);
+		}while (ch == '\n');
 		}
 }
 fclose(fp);
