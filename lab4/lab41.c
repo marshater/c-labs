54259899bb805849cc9653703bda731096b03eb0
Evgeniy Belyaev <44948688+marshater@users.noreply.github.com>
2019-09-15 23:44:23 +0700

    Create lab41.c

---
+++ lab4/lab41.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define lim 100
+
+void removing(FILE*, FILE*, int);
+int main(int argc, char *argv[])
+{
+	  if (argc < 2){
+	fprintf (stderr, "Мало аргументов. Используйте <имя файла> <удаляемый символ>\n");
+}
+
+	FILE *fp;
+	FILE *stream;
+	printf("%s\n", argv[1]);
+	printf("%i\n", atoi(argv[2]));
+	fp = fopen(argv[1], "r");
+	stream = fopen("editing", "w");
+	removing(fp, stream, atoi(argv[2]));
+	fclose(fp);
+	fclose(stream);
+	return 0;
+}
+void removing(FILE *fp, FILE *stream, int num){
+	char ch;
+	int count = 0;
+	char *p = NULL;
+	p = (char*)calloc(100, sizeof(char));
+	while ((ch=fgetc(fp)) != EOF){
+		if(ch != '\n'){
+			p[count] = ch;
+			count++;
+			}
+			else if(count < num){
+				fputs(p, stream);
+				count = 0;
+				memset(p, 0, sizeof(p));}else{
+				count = 0;
+				memset(p, 0, sizeof(p));}
+			};
+			free (p);
+}
