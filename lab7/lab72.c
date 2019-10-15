5c2a59bff87edfb2669db363ef11319405803467
Evgeniy Belyaev <44948688+marshater@users.noreply.github.com>
2019-10-16 01:38:07 +0700

    Update lab72.c

--- lab7/lab72.c
+++ lab7/lab72.c
@@ -14,22 +14,22 @@ void arrFree(int ** arr, int dim);
 
 
 int mult(int A, int B, int N) {
-	int res = 0;
+    int res = 0;
     res += A * B;
-  return res;
+    return res;
 }
 
 void arrFill(int **A, int N) {
-	for(int i = 0; i < N; i++) {
-		for(int j = 0; j < N; j++) {
-			A[i][j] = 1 + rand() % 5;
-		}
-	}
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            A[i][j] = 1 + rand() % 5;
+        }
+    }
 }
 void VecFill(int *B, int N) {
-	for(int i = 0; i < N; i++) {
-			B[i] = 1 + rand() % 5;
-	}
+    for(int i = 0; i < N; i++) {
+            B[i] = 1 + rand() % 5;
+    }
 }
 
 
@@ -44,119 +44,116 @@ int doit(int **A, int *B, int *M, int N) {
     for (int i = 0; i < N; i++) {
         pid[i] = fork();
         if (pid < 0) {
-          printf("fork() failed\n");
-          exit(-1);
+            printf("fork() failed\n");
+            exit(-1);
         } else if (pid[i] == 0) {
-			for(int i = 0; i < N; i++){
-			  for(int j = 0; j < N; j++){
-				int tmp = mult(A[j][i], B[j], N);
-                R = R + tmp;   
-		      }
-		      fd[i] = open(FIFO1, O_RDWR, 0);
-		      write(fd[i], &R, sizeof(int));
-		      close(fd[i]);
-		      printf("child: C[%d]=%d \t",i, R);
-		      R = 0;
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+                    int tmp = mult(A[j][i], B[j], N);
+                    R = R + tmp;
+                }
+                fd[i] = open(FIFO1, O_RDWR, 0);
+                write(fd[i], &R, sizeof(int));
+                close(fd[i]);
+                printf("child: C[%d]=%d \t",i, R);
+                R = 0;
             }
-	      }
-	  exit(0);
+        }
+        exit(0);
     }
-for(int i = 0; i < N; i++) {
-	  	for (int i = 0; i < N; i++) {
-		  pfd[i]=open(FIFO1, O_RDONLY, 0);
-		  int res;
-		  if (pid[i]==waitpid(pid[i], &status, 0)) {
-           	read(pfd[i], &res, sizeof(int));
-           	close(pfd[i]);
-		}
-	}
-
-  }
-	return 0;
+        for(int i = 0; i < N; i++) {
+            for (int i = 0; i < N; i++) {
+                pfd[i]=open(FIFO1, O_RDONLY, 0);
+                int res;
+                if (pid[i]==waitpid(pid[i], &status, 0)) {
+                    read(pfd[i], &res, sizeof(int));
+                    close(pfd[i]);
+                }
+        }
+    }
+    return 0;
 }
 
 void arrInit(int *** arr, int dim) {
-  fprintf(stderr, "dbg: %s\n", __func__);
-  if (!arr) return;
-
-  int ** arrTmp = calloc(dim, sizeof(int*));
-  if (!arrTmp) return;
-
-  for (int i = 0; i < dim; ++i) {
-    arrTmp[i] = calloc(dim, sizeof(int));
-    if (!arrTmp[i]) return; 
-  }
+    fprintf(stderr, "dbg: %s\n", __func__);
+    if (!arr) return;
+    int ** arrTmp = calloc(dim, sizeof(int*));
+    if (!arrTmp) return;
+    for (int i = 0; i < dim; ++i) {
+        arrTmp[i] = calloc(dim, sizeof(int));
+        if (!arrTmp[i]) return; 
+    }
 
   *arr = arrTmp;  
 }
+
 void VecInit(int * Vec, int dim) {
-  fprintf(stderr, "dbg: %s\n", __func__);
-  if (!Vec) return;
-  Vec = calloc(dim, sizeof(int*));
-  if (!Vec) return;
+    fprintf(stderr, "dbg: %s\n", __func__);
+    if (!Vec) return;
+    Vec = calloc(dim, sizeof(int*));
+    if (!Vec) return;
 }
 
 void arrFree(int ** arr, int dim) {
-  fprintf(stderr, "dbg: %s\n", __func__);
-  if (!arr) return;
-  for (int i = 0; i < dim; ++i) free(arr[i]);
-  free(arr); 
+    fprintf(stderr, "dbg: %s\n", __func__);
+    if (!arr) return;
+    for (int i = 0; i < dim; ++i) free(arr[i]);
+    free(arr); 
 }
+
 void VecFree(int * arr, int dim) {
-  fprintf(stderr, "dbg: %s\n", __func__);
-  if (!arr) return;
-  free(arr); 
+    fprintf(stderr, "dbg: %s\n", __func__);
+    if (!arr) return;
+    free(arr); 
 }
 
 void arrPrint(int ** arr, int dim) {
-  fprintf(stderr, "dbg: %s\n", __func__);
-  if (!arr) return;
-
-  for (int i = 0; i < dim; ++i) {
-    for (int j = 0; j < dim; ++j) {
-    printf("%2d ", arr[i][j]);
+    fprintf(stderr, "dbg: %s\n", __func__);
+    if (!arr) return;
+        for (int i = 0; i < dim; ++i) {
+            for (int j = 0; j < dim; ++j) {
+            printf("%2d ", arr[i][j]);
+        }
+        printf("\n"); 
     }
-    printf("\n"); 
-  }
   printf("\n");
 }
 
 
 void VecPrint(int *arr, int dim) {
-  fprintf(stderr, "dbg: %s\n", __func__);
-  if (!arr) return;
-  for (int i = 0; i < dim; i++) {
-    printf("%i", arr[i]);
-	printf("\n");
-  }
+    fprintf(stderr, "dbg: %s\n", __func__);
+    if (!arr) return;
+    for (int i = 0; i < dim; i++){
+        printf("%i", arr[i]);
+        printf("\n");
+    }
 }
 
 int main(int argc, char** argv) {
-  int **A, *B, *M, N;
-  srand(time(NULL));
-  scanf("%d", &N);
-  if (N <= 0) return -1;
-  B = calloc(N, sizeof(int*));
-  M = calloc(N, sizeof(int*));
+    int **A, *B, *M, N;
+    srand(time(NULL));
+    scanf("%d", &N);
+    if (N <= 0) return -1;
+    B = calloc(N, sizeof(int*));
+    M = calloc(N, sizeof(int*));
 
-  arrInit(&A, N);
+    arrInit(&A, N);
 
-	arrFill(A,N);
-	VecFill(B,N);
+    arrFill(A,N);
+    VecFill(B,N);
 
-  printf("Arr A:\n");
-  arrPrint(A, N);
+    printf("Arr A:\n");
+    arrPrint(A, N);
 
-  printf("Arr B:\n");
-  VecPrint(B, N);
+    printf("Arr B:\n");
+    VecPrint(B, N);
 
-	doit(A, B, M, N);
+    doit(A, B, M, N);
+    printf("Arr M = A x B =\n");
+    VecPrint(M, N);
 
-  printf("Arr M = A x B =\n");
-	VecPrint(M, N);
-
-  arrFree(A, N);
-	free(B);
-	free(M);
+    arrFree(A, N);
+    free(B);
+    free(M);
   return 0;
 }
