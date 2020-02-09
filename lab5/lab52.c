2f552bea986c24cd81da8da7a76150ab02a1f206
Evgeniy Belyaev <44948688+marshater@users.noreply.github.com>
2020-02-09 21:49:01 +0700

    Update lab52.c

--- lab5/lab52.c
+++ lab5/lab52.c
@@ -1,26 +1,3 @@
-/*
- * lab52.c
- * 
- * Copyright 2019 PEKA <peka@peka-VirtualBox>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
- */
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,12 +6,10 @@
 int main(int argc, char **argv)
 {
 	void *handler;
-
 	handler = dlopen("/home/usre/Загрузки/libmath2.so",RTLD_LAZY);
-	if (!handler)
-	{
-	fprintf(stderr,"dlopen() error: %s\n", dlerror());
-	exit(1);
+	if (!handler){
+		fprintf(stderr,"dlopen() error: %s\n", dlerror());
+		exit(1);
 	}
 	int (*mult)(int,int) = dlsym(handler,"multiply");
 	int (*div)(int,int) = dlsym(handler,"divide");
