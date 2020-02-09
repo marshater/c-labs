b8cb905a16c71fc1597d2db09cb4d3897d6a04b0
Evgeniy Belyaev <44948688+marshater@users.noreply.github.com>
2020-02-09 21:47:02 +0700

    Update lab42.c

--- lab4/lab42.c
+++ lab4/lab42.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
-	  if (argc < 3){
+int main(int argc, char *argv[]){
+	if (argc < 3){
 	fprintf (stderr, "Мало аргументов. Используйте <имя файла> <удаляемый символ>\n");
-}
+	}
 
 	FILE *fp;
 	FILE *stream;
 	fp = fopen(argv[1], "r");
 	stream = fopen("editing", "w");
 	if((fp=fopen(argv[1], "r"))==NULL||(stream=fopen("editing", "w"))==NULL) {
-    	printf("Не удается открыть файл.\n");
-    	exit(1);
+    		printf("Не удается открыть файл.\n");
+    		exit(1);
 	}
 	deleting(fp, stream, *argv[2]);
 	fclose(fp);
@@ -24,6 +23,6 @@ void deleting(FILE *fp, FILE *stream, char del){
 	while ((ch=fgetc(fp)) != EOF){
 		if(ch != del){
 			fputc(ch, stream);
-			};
-}
+		};
+	}
 }
