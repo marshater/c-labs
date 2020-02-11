1dc3f3026c363cc199f652afd215bb1cea7a2465
Evgeniy Belyaev <44948688+marshater@users.noreply.github.com>
2020-02-11 23:00:00 +0700

    Update lab101.c

--- lab10/lab101.c
+++ lab10/lab101.c
@@ -130,7 +130,7 @@ for (int i = 0; i < N; i++){
 	for (int j = 0; j < N; j++){
 // грязный костыль для ввода построчно
 		info.SubMatrix[j] = B[j];
-        	info.Number = N;
+        info.Number = N;
 		info.MainMatrix[j] = A[j][i];
 	}
 // вы мутехи, да? Если мутехи, то идите найух:)
@@ -143,6 +143,7 @@ for (int i = 0; i < N; i++){
         }
         if ((pthread_join(lines[i],(void**) &R)) != 0){
             perror("Cant join");
+			pthread_mutex_destroy(&mutex);
             return EXIT_FAILURE;
         } else {
     	printf("%i\n", (int) *R);
@@ -161,7 +162,7 @@ for (int i = 0; i < N; i++){
     return 0;
 }
 
-    void *MultMatrix(void *arg){
+void *MultMatrix(void *arg){
     int Z = 0;
     struct data* info = (struct data*) arg;
     for (int i = 0; i < info->Number; i++){
