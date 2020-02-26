7e0850bed877210ea4cf43e796a5d0b9842993de
Evgeniy Belyaev <44948688+marshater@users.noreply.github.com>
2020-02-26 21:37:00 +0700

    Update lab51.c

--- lab5/lab51.c
+++ lab5/lab51.c
@@ -1,11 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "math.h"
-int main(int argc, char **argv){
-	if(argc<4){
+int main(int argc, char **argv)
+{
+	if(argc<4)
+	{
 		printf("too few arg\n"); exit(1);
 	}
+	
 	printf("%i\n",multiply(atoi(argv[2]),atoi(argv[4])));
 	printf("%f",divide(atoi(argv[2]),atoi(argv[4])));
+	
 	return 0;
 }
