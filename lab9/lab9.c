2e5a64b746b6931a188fea42f16b24e84f9f071b
Evgeniy Belyaev <44948688+marshater@users.noreply.github.com>
2020-02-26 22:06:46 +0700

    Update lab9.c

--- lab9/lab9.c
+++ lab9/lab9.c
@@ -9,6 +9,16 @@
 #include <fcntl.h>
 #include <string.h>
 #include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/shm.h>
@@ -17,14 +27,16 @@
 
 #define MAX_SEND_SIZE 80
 
-union semun {
+union semun 
+{
 	int val;
 	struct semid_ds *buf;
 	unsigned short *array;
 	struct seminfo *__buf;
 };
 
-struct msgbuf {
+struct msgbuf 
+{
 	long type;
 	int msg;
 };
@@ -33,42 +45,54 @@ void arrInit(int *** arr, int dim);
 void arrFree(int ** arr, int dim);
 
 
-int mult(int A, int B) {
+int mult(int A, int B) 
+{
 	return A * B;
 }
 
-void arrFill(int **A, int N) {
-	for(int i = 0; i < N; i++) {
-		for(int j = 0; j < N; j++) {
+void arrFill(int **A, int N) 
+{
+	for(int i = 0; i < N; i++) 
+	{
+		for(int j = 0; j < N; j++) 
+		{
 			A[i][j] = 1 + rand() % 5;
 		}
 	}
 }
-void vecFill(int *B, int N) {
-	for(int i = 0; i < N; i++) {
-			B[i] = 1 + rand() % 5;
+
+void vecFill(int *B, int N) 
+{
+	for(int i = 0; i < N; i++) 
+	{
+		B[i] = 1 + rand() % 5;
 	}
 }
 
-void vecPrint(int *arr, int dim) {
+void vecPrint(int *arr, int dim)
+{
 	fprintf(stderr, "dbg: %s\n", __func__);
 	if (!arr) return;
-	for (int i = 0; i < dim; i++) {
+	for (int i = 0; i < dim; i++)
+	{
 		printf("%i", arr[i]);
 		printf("\n");
 	}
 	printf("\n");
 }
 
-void doit(int **A, int *B, int N, int *shmem, int key ) {
+void doit(int **A, int *B, int N, int *shmem, int key )
+{
 	pid_t pid[N];
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++) 
+	{
 		pid[i] = fork();
-
-		if (pid < 0) {
+		if (pid < 0) 
+		{
 			printf("fork() failed\n");
 			exit(-1);
-		} else if (pid[i] == 0) {
+		} else if (pid[i] == 0) 
+		{
 			int semid;
 			union semun arg;
 			struct sembuf lock_res = {0, -1, 0};
@@ -79,18 +103,21 @@ void doit(int **A, int *B, int N, int *shmem, int key ) {
 
 			int R = 0;
 
-			for(int j = 0; j < N; j++){
+			for(int j = 0; j < N; j++)
+			{
 				R += mult(A[j][i], B[j]);
 			}
 
-			if((semop(semid, &lock_res, 1)) == -1){
+			if((semop(semid, &lock_res, 1)) == -1)
+			{
 					fprintf(stderr, "Lock failed\n");
 					exit(1);
 			}
 
 				shmem[i] = R;
 
-			if((semop(semid, &rel_res, 1)) == -1){
+			if((semop(semid, &rel_res, 1)) == -1)
+			{
 			fprintf(stderr, "Unlock failed\n");
 			exit(6);
 			} else {
@@ -103,25 +130,28 @@ void doit(int **A, int *B, int N, int *shmem, int key ) {
 	}
 	int status;
 	for(int i = 0; i < N; i++) {
-		if (pid[i] == waitpid(pid[i], &status, 0)) {
+		if (pid[i] == waitpid(pid[i], &status, 0))
+		{
 		}
 	}
 }
 
-void arrInit(int *** arr, int dim) {
+void arrInit(int *** arr, int dim) 
+{
 	fprintf(stderr, "dbg: %s\n", __func__);
 	if (!arr) return;
 	int ** arrTmp = calloc(dim, sizeof(int*));
 	if (!arrTmp) return;
-	for (int i = 0; i < dim; ++i) {
+	for (int i = 0; i < dim; ++i) 
+	{
 		arrTmp[i] = calloc(dim, sizeof(int));
 		if (!arrTmp[i]) return; 
 	}
-
   *arr = arrTmp;  
 }
 
-void vecInit(int **vec, int dim) {
+void vecInit(int **vec, int dim) 
+{
 	fprintf(stderr, "dbg: %s\n", __func__);
 	if (!vec) return;
 	int *vecTmp = calloc(dim, sizeof(int*));
@@ -129,24 +159,29 @@ void vecInit(int **vec, int dim) {
 	*vec = vecTmp;
 }
 
-void arrFree(int ** arr, int dim) {
+void arrFree(int ** arr, int dim)
+{
 	fprintf(stderr, "dbg: %s\n", __func__);
 	if (!arr) return;
 	for (int i = 0; i < dim; ++i) free(arr[i]);
 	free(arr); 
 }
 
-void vecFree(int * arr, int dim) {
+void vecFree(int * arr, int dim) 
+{
 	fprintf(stderr, "dbg: %s\n", __func__);
 	if (!arr) return;
 	free(arr); 
 }
 
-void arrPrint(int ** arr, int dim) {
+void arrPrint(int ** arr, int dim) 
+{
 	fprintf(stderr, "dbg: %s\n", __func__);
 	if (!arr) return;
-		for (int i = 0; i < dim; ++i) {
-			for (int j = 0; j < dim; ++j) {
+	for (int i = 0; i < dim; ++i) 
+	{
+		for (int j = 0; j < dim; ++j) 
+		{
 			printf("%2d ", arr[i][j]);
 		}
 		printf("\n"); 
@@ -154,7 +189,8 @@ void arrPrint(int ** arr, int dim) {
 	printf("\n");
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) 
+{
 	int **A, *B, N;
 	int shid, *shmem;
 	key_t key;
@@ -165,12 +201,14 @@ int main(int argc, char** argv) {
 	scanf("%d", &N);
 	if (N <= 0) return -1;
 
-	if((shid = shmget(key, N*sizeof(long), 0666 | IPC_CREAT))< 0){
+	if((shid = shmget(key, N*sizeof(long), 0666 | IPC_CREAT))< 0)
+	{
 		perror("shmget failure");
 		exit(1);
 	}
 
-	if ((shmem = shmat(shid, NULL, 0)) == (int *) -1) {
+	if ((shmem = shmat(shid, NULL, 0)) == (int *) -1) 
+	{
 		perror("shmat failure");
 		exit(2);
 	}
@@ -195,7 +233,8 @@ int main(int argc, char** argv) {
 	vecFree(B, N);
 
 
-	 if (shmctl(shid, IPC_RMID, 0) < 0) {
+	 if (shmctl(shid, IPC_RMID, 0) < 0) 
+	{
 		perror("shmem deleting failure\n");
 		exit(1);
 	}
