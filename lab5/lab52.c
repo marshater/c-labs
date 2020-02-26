1e7644facb8580ab8f701d02efdfbe06ebdc8a36
Evgeniy Belyaev <44948688+marshater@users.noreply.github.com>
2020-02-26 21:37:45 +0700

    Update lab52.c

--- lab5/lab52.c
+++ lab5/lab52.c
@@ -7,15 +7,21 @@ int main(int argc, char **argv)
 {
 	void *handler;
 	handler = dlopen("/home/usre/Загрузки/libmath2.so",RTLD_LAZY);
-	if (!handler){
+	
+	if (!handler)
+	{
 		fprintf(stderr,"dlopen() error: %s\n", dlerror());
 		exit(1);
 	}
+	
 	int (*mult)(int,int) = dlsym(handler,"multiply");
 	int (*div)(int,int) = dlsym(handler,"divide");
+	
 	printf("%d\n",mult(atoi(argv[2]), atoi(argv[4])));
 	printf("%d",div(atoi(argv[2]), atoi(argv[4])));
+	
 	dlclose(handler);
+	
 	return 0;
 }
 
