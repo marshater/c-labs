b02e78d1221301d4734802a023659a0ce7712c9d
Evgeniy Belyaev <44948688+marshater@users.noreply.github.com>
2020-02-26 21:31:56 +0700

    Update lab41.c

--- lab4/lab41.c
+++ lab4/lab41.c
@@ -1,44 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#DEFINE LIM 100
+
+#define LIM 100
 
 void removing(FILE*, FILE*, int);
-int main(int argc, char *argv[]){
-	if (argc < 2){
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
 	fprintf (stderr, "Мало аргументов. Используйте <имя файла> <удаляемый символ>\n");
 	}
+
 	FILE *fp;
 	FILE *stream;
+
 	fp = fopen(argv[1], "r");
 	stream = fopen("editing", "w");
-		if((fp=fopen(argv[1], "r"))==NULL||(stream=fopen("editing", "w"))==NULL) {
-    			printf("Не удается открыть файл.\n");
-    			exit(1);
-		}
+	if((fp=fopen(argv[1], "r"))==NULL||(stream=fopen("editing", "w"))==NULL) 
+	{
+    	printf("Не удается открыть файл.\n");
+    	exit(1);
+	}
+
 	removing(fp, stream, atoi(argv[2]));
+
 	fclose(fp);
 	fclose(stream);
+
 	return 0;
 }
-void removing(FILE *fp, FILE *stream, int num){
+void removing(FILE *fp, FILE *stream, int num)
+{
 	char ch;
 	int count = 0;
 	char *p = NULL;
 	p = (char*)calloc(LIM, sizeof(char));
-	while ((ch=fgetc(fp)) != EOF){
-		if(ch != '\n'){
+	while ((ch=fgetc(fp)) != EOF)
+	{
+		if(ch != '\n')
+		{
 			p[count] = ch;
 			count++;
 		}
-			else if(count < num){
+			else if(count < num)
+			{
 				fputs(p, stream);
 				fputs("\n", stream);
 				count = 0;
-				memset(p, 0, LIM);}
-			else{
+				memset(p, 0, LIM);
+			}
+			else
+			{
 				count = 0;
-				memset(p, 0, LIM);}
-			};
-			free (p);
+				memset(p, 0, LIM);
+			}
+	};
+	free (p);
 }
